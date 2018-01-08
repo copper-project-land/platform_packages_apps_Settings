nitrogen-project/android_packages_apps_Settings

SoundSettings: Add charging vibration settings
Change-Id: Ie66f9261037bdab87b86cd49b558799decccd3b8

src/com/android/settings/notification/ChargingVibroPreferenceController.java
@@ -1,0 +1,39 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.notification;
+
+import static com.android.settings.notification.SettingPref.TYPE_GLOBAL;
+
+import android.content.Context;
+
+import android.provider.Settings.Global;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.core.lifecycle.Lifecycle;
+
+public class ChargingVibroPreferenceController extends SettingPrefController {
+
+    private static final String KEY_CHARGING_VIBRO = "charging_vibro";
+
+    public ChargingVibroPreferenceController(Context context, SettingsPreferenceFragment parent,
+            Lifecycle lifecycle) {
+        super(context, parent, lifecycle);
+        mPreference = new SettingPref(
+            TYPE_GLOBAL, KEY_CHARGING_VIBRO, Global.POWER_NOTIFICATIONS_VIBRATE, DEFAULT_ON);
+
+    }
+
+}

copper-project-land/platform_packages_apps_Settings

src/com/android/settings/notification/ChargingVibroPreferenceController.java
@@ -1,0 +1,39 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.notification;
+
+import static com.android.settings.notification.SettingPref.TYPE_GLOBAL;
+
+import android.content.Context;
+
+import android.provider.Settings.Global;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+
+public class ChargingVibroPreferenceController extends SettingPrefController {
+
+    private static final String KEY_CHARGING_VIBRO = "charging_vibro";
+
+    public ChargingVibroPreferenceController(Context context, SettingsPreferenceFragment parent,
+            Lifecycle lifecycle) {
+        super(context, parent, lifecycle);
+        mPreference = new SettingPref(
+            TYPE_GLOBAL, KEY_CHARGING_VIBRO, Global.POWER_NOTIFICATIONS_VIBRATE, DEFAULT_ON);
+
+    }
+
+}