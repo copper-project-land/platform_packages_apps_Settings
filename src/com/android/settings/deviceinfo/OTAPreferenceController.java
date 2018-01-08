nitrogen-project/android_packages_apps_Settings

Add Nitrogen stuff *Nitrogen logo *Nitrogen OTA *Remove legal info
Change-Id: If0e10c2717d8a17916511e7767f0a98687c774d9

Signed-off-by: xyyx <xyyx@mail.ru>

src/com/android/settings/deviceinfo/OTAPreferenceController.java
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo;
+
+import android.content.Context;
+
+import com.android.settings.R;
+import com.android.settings.core.PreferenceController;
+
+import com.android.settings.development.DevelopmentSettings;
+
+public class OTAPreferenceController extends PreferenceController {
+
+    private static final String KEY_NOS_UPDATES = "nos_updates";
+    Context mContext;
+
+    public OTAPreferenceController(Context context) {
+        super(context);
+        mContext = context;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return DevelopmentSettings.isPackageInstalled(mContext, "com.nitrogen.ota");
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_NOS_UPDATES;
+    }
+}

copper-project-land/platform_packages_apps_Settings

Conflicts:
	src/com/android/settings/DeviceInfoSettings.java

src/com/android/settings/deviceinfo/OTAPreferenceController.java
@@ -1,0 +1,47 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo;
+
+import android.content.Context;
+
+import com.android.settings.R;
+
+import com.android.settings.development.DevelopmentSettings;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+public class OTAPreferenceController extends AbstractPreferenceController
+                implements PreferenceControllerMixin {
+
+    private static final String KEY_NOS_UPDATES = "nos_updates";
+    Context mContext;
+
+    public OTAPreferenceController(Context context) {
+        super(context);
+        mContext = context;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return DevelopmentSettings.isPackageInstalled(mContext, "com.nitrogen.ota");
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_NOS_UPDATES;
+    }
+}