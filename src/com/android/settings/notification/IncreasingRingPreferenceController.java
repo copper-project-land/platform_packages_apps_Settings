LineageOS/android_packages_apps_Settings

Add back increasing ring feature (2/3).
This also squashes in the following fixes from cm-13.0:
Ia4291d8ac3d1dbf76d432db7e0e973844163c4ae Fix NPE.
Ibe7fdf1ee7f0aa1c7c76052df69fa92e8b6a2ffd Fix up increasing ring tone volume preview behaviour.
Id32d6b51aab05e80be59bc1912c4ae7c5d114ac5 Settings: Always set ramp-up time value during bindView

Change-Id: Iafc79a57651db327a100e0f1723b62f442b638be

src/com/android/settings/notification/IncreasingRingPreferenceController.java
@@ -1,0 +1,41 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.notification;
+
+import android.content.Context;
+
+import com.android.settings.Utils;
+import com.android.settings.core.PreferenceController;
+
+public class IncreasingRingPreferenceController extends PreferenceController {
+
+    private static final String KEY_INCREASING_RING = "increasing_ring";
+
+    public DialPadTonePreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_INCREASING_RING;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return Utils.isVoiceCapable(mContext);
+    }
+}

src/com/android/settings/notification/IncreasingRingVolumePreferenceController.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.notification;
+
+import android.content.Context;
+import android.support.v7.preference.PreferenceScreen;
+
+import com.android.settings.core.lifecycle.Lifecycle;
+import com.android.settings.core.lifecycle.LifecycleObserver;
+import com.android.settings.core.lifecycle.events.OnPause;
+import com.android.settings.core.lifecycle.events.OnResume;
+import com.android.settings.core.lifecycle.events.OnStop;
+
+/**
+ * Base class for preference controller that handles VolumeSeekBarPreference
+ */
+public abstract class IncreasingRingVolumePreferenceController
+        extends AdjustVolumeRestrictedPreferenceController
+        implements LifecycleObserver, OnResume, OnStop {
+
+    private static final String KEY_INCREASING_RING_VOLUME = "increasing_ring_volume";
+
+    private IncreasingRingVolumePreference mPreference;
+    private IncreasingRingVolumePreference.Callback mCallback;
+    private AudioHelper mHelper;
+
+    public VolumeSeekBarPreferenceController(Context context,
+            IncreasingRingVolumePreference.Callback callback, Lifecycle lifecycle) {
+        super(context);
+        mCallback = callback;
+        mHelper = new AudioHelper(context);
+        if (lifecycle != null) {
+            lifecycle.addObserver(this);
+        }
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        if (isAvailable()) {
+            final String key = getPreferenceKey();
+            mPreference = (IncreasingRingVolumePreference) screen.findPreference(key);
+            mPreference.setCallback(mCallback);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        if (mPreference != null) {
+            mPreference.onActivityResume();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        if (mPreference != null) {
+            mPreference.onActivityStop();
+        }
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_RING_VOLUME;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return Utils.isVoiceCapable(mContext) && !mHelper.isSingleVolume();
+    }
+}

src/com/android/settings/notification/SoundSettings.java
@@ -50,6 +50,8 @@ public class SoundSettings extends DashboardFragment {
     private static final int SAMPLE_CUTOFF = 2000;  // manually cap sample playback at 2 seconds
 
     private final VolumePreferenceCallback mVolumeCallback = new VolumePreferenceCallback();
+    private final IncreasingRingVolumePreferenceCallback mIncreasingRingVolumeCallback =
+        new IncreasingRingVolumePreferenceCallback();
     private final H mHandler = new H();
 
     private RingtonePreference mRequestPreference;
@@ -85,6 +87,7 @@ public class SoundSettings extends DashboardFragment {
     public void onPause() {
         super.onPause();
         mVolumeCallback.stopSample();
+        mIncreasingVolumeCallback.stopSample();
     }
 
     @Override
@@ -114,7 +117,8 @@ public class SoundSettings extends DashboardFragment {
 
     @Override
     protected List<PreferenceController> getPreferenceControllers(Context context) {
-        return buildPreferenceControllers(context, this, mVolumeCallback, getLifecycle());
+        return buildPreferenceControllers(context, this, mVolumeCallback,
+                mIncreasingRingVolumeCallback, getLifecycle());
     }
 
     @Override
@@ -143,6 +147,7 @@ public class SoundSettings extends DashboardFragment {
             if (mCurrent != null && mCurrent != sbv) {
                 mCurrent.stopSample();
             }
+            mIncreasingVolumeCallback.stopSample();
             mCurrent = sbv;
             if (mCurrent != null) {
                 mHandler.removeMessages(H.STOP_SAMPLE);
@@ -162,6 +167,26 @@ public class SoundSettings extends DashboardFragment {
         }
     }
 
+    final class IncreasingRingVolumePreferenceCallback implements
+            IncreasingRingVolumePreference.Callback {
+        private IncreasingRingVolumePreference mPlayingPref;
+
+        @Override
+        public void onSampleStarting(IncreasingRingVolumePreference pref) {
+            mPlayingPref = pref;
+            mVolumeCallback.stopSample();
+            mHandler.removeMessages(H.STOP_SAMPLE);
+            mHandler.sendEmptyMessageDelayed(H.STOP_SAMPLE, SAMPLE_CUTOFF);
+        }
+
+        public void stopSample() {
+            if (mPlayingPref != null) {
+                mPlayingPref.stopSample();
+                mPlayingPref = null;
+            }
+        }
+    };
+
     // === Callbacks ===
 
 
@@ -177,6 +202,7 @@ public class SoundSettings extends DashboardFragment {
             switch (msg.what) {
                 case STOP_SAMPLE:
                     mVolumeCallback.stopSample();
+                    mIncreasingVolumeCallback.stopSample();
                     break;
             }
         }
@@ -184,6 +210,7 @@ public class SoundSettings extends DashboardFragment {
 
     private static List<PreferenceController> buildPreferenceControllers(Context context,
             SoundSettings fragment, VolumeSeekBarPreference.Callback callback,
+            IncreasingRingVolumePreference.Callback incCallback,
             Lifecycle lifecycle) {
         final List<PreferenceController> controllers = new ArrayList<>();
         controllers.add(new ZenModePreferenceController(context));
@@ -197,6 +224,9 @@ public class SoundSettings extends DashboardFragment {
         controllers.add(
                 new NotificationVolumePreferenceController(context, callback, lifecycle));
         controllers.add(new RingVolumePreferenceController(context, callback, lifecycle));
+        controllers.add(new IncreasingRingPreferenceController(context));
+        controllers.add(new IncreasingRingVolumePreferenceController(
+                    context, incCallback, lifecycle));
 
         // === Phone & notification ringtone ===
         controllers.add(new PhoneRingtonePreferenceController(context));
@@ -235,7 +265,7 @@ public class SoundSettings extends DashboardFragment {
                 @Override
                 public List<PreferenceController> getPreferenceControllers(Context context) {
                     return buildPreferenceControllers(context, null /* fragment */,
-                            null /* callback */, null /* lifecycle */);
+                            null /* callback */, null, /* incCallback */, null /* lifecycle */);
                 }
 
                 @Override

copper-project-land/platform_packages_apps_Settings

src/com/android/settings/notification/IncreasingRingPreferenceController.java
@@ -1,0 +1,43 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.notification;
+
+import android.content.Context;
+
+import com.android.settings.Utils;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+public class IncreasingRingPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin {
+
+    private static final String KEY_INCREASING_RING = "increasing_ring";
+
+    public IncreasingRingPreferenceController(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_INCREASING_RING;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return Utils.isVoiceCapable(mContext);
+    }
+}

src/com/android/settings/notification/IncreasingRingVolumePreferenceController.java
@@ -1,0 +1,85 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.notification;
+
+import android.content.Context;
+import android.support.v7.preference.PreferenceScreen;
+
+import com.android.settings.Utils;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+import com.android.settingslib.core.lifecycle.LifecycleObserver;
+import com.android.settingslib.core.lifecycle.events.OnPause;
+import com.android.settingslib.core.lifecycle.events.OnResume;
+import com.android.settingslib.core.lifecycle.events.OnStop;
+
+/**
+ * Base class for preference controller that handles VolumeSeekBarPreference
+ */
+public class IncreasingRingVolumePreferenceController
+        extends AdjustVolumeRestrictedPreferenceController
+        implements LifecycleObserver, OnResume, OnStop {
+
+    private static final String KEY_INCREASING_RING_VOLUME = "increasing_ring_volume";
+
+    private IncreasingRingVolumePreference mPreference;
+    private IncreasingRingVolumePreference.Callback mCallback;
+    private AudioHelper mHelper;
+
+    public IncreasingRingVolumePreferenceController(Context context,
+            IncreasingRingVolumePreference.Callback callback, Lifecycle lifecycle) {
+        super(context);
+        mCallback = callback;
+        mHelper = new AudioHelper(context);
+        if (lifecycle != null) {
+            lifecycle.addObserver(this);
+        }
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        if (isAvailable()) {
+            final String key = getPreferenceKey();
+            mPreference = (IncreasingRingVolumePreference) screen.findPreference(key);
+            mPreference.setCallback(mCallback);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        if (mPreference != null) {
+            mPreference.onActivityResume();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        if (mPreference != null) {
+            mPreference.onActivityStop();
+        }
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_INCREASING_RING_VOLUME;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return Utils.isVoiceCapable(mContext) && !mHelper.isSingleVolume();
+    }
+}

src/com/android/settings/notification/SoundSettings.java
@@ -57,6 +57,8 @@ public class SoundSettings extends DashboardFragment {
     private static final int SAMPLE_CUTOFF = 2000;  // manually cap sample playback at 2 seconds
 
     private final VolumePreferenceCallback mVolumeCallback = new VolumePreferenceCallback();
+    private final IncreasingRingVolumePreferenceCallback mIncreasingRingVolumeCallback =
+        new IncreasingRingVolumePreferenceCallback();
     private final H mHandler = new H();
 
     private RingtonePreference mRequestPreference;
@@ -100,6 +102,7 @@ public class SoundSettings extends DashboardFragment {
     public void onPause() {
         super.onPause();
         mVolumeCallback.stopSample();
+        mIncreasingRingVolumeCallback.stopSample();
     }
 
     @Override
@@ -134,7 +137,8 @@ public class SoundSettings extends DashboardFragment {
 
     @Override
     protected List<AbstractPreferenceController> getPreferenceControllers(Context context) {
-        return buildPreferenceControllers(context, this, mVolumeCallback, getLifecycle());
+        return buildPreferenceControllers(context, this, mVolumeCallback,
+                mIncreasingRingVolumeCallback, getLifecycle());
     }
 
     @Override
@@ -163,6 +167,7 @@ public class SoundSettings extends DashboardFragment {
             if (mCurrent != null && mCurrent != sbv) {
                 mCurrent.stopSample();
             }
+            mIncreasingRingVolumeCallback.stopSample();
             mCurrent = sbv;
             if (mCurrent != null) {
                 mHandler.removeMessages(H.STOP_SAMPLE);
@@ -186,6 +191,26 @@ public class SoundSettings extends DashboardFragment {
         }
     }
 
+    final class IncreasingRingVolumePreferenceCallback implements
+            IncreasingRingVolumePreference.Callback {
+        private IncreasingRingVolumePreference mPlayingPref;
+
+        @Override
+        public void onSampleStarting(IncreasingRingVolumePreference pref) {
+            mPlayingPref = pref;
+            mVolumeCallback.stopSample();
+            mHandler.removeMessages(H.STOP_SAMPLE);
+            mHandler.sendEmptyMessageDelayed(H.STOP_SAMPLE, SAMPLE_CUTOFF);
+        }
+
+        public void stopSample() {
+            if (mPlayingPref != null) {
+                mPlayingPref.stopSample();
+                mPlayingPref = null;
+            }
+        }
+    };
+
     // === Callbacks ===
 
 
@@ -201,6 +226,7 @@ public class SoundSettings extends DashboardFragment {
             switch (msg.what) {
                 case STOP_SAMPLE:
                     mVolumeCallback.stopSample();
+                    mIncreasingRingVolumeCallback.stopSample();
                     break;
             }
         }
@@ -208,6 +234,7 @@ public class SoundSettings extends DashboardFragment {
 
     private static List<AbstractPreferenceController> buildPreferenceControllers(Context context,
             SoundSettings fragment, VolumeSeekBarPreference.Callback callback,
+            IncreasingRingVolumePreference.Callback incCallback,
             Lifecycle lifecycle) {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(new ZenModePreferenceController(context));
@@ -220,6 +247,9 @@ public class SoundSettings extends DashboardFragment {
         controllers.add(new MediaVolumePreferenceController(context, callback, lifecycle));
         controllers.add(new NotificationVolumePreferenceController(context, callback, lifecycle));
         controllers.add(new RingVolumePreferenceController(context, callback, lifecycle));
+        controllers.add(new IncreasingRingPreferenceController(context));
+        controllers.add(new IncreasingRingVolumePreferenceController(
+                    context, incCallback, lifecycle));
 
         // === Phone & notification ringtone ===
         controllers.add(new PhoneRingtonePreferenceController(context));
@@ -300,7 +330,7 @@ public class SoundSettings extends DashboardFragment {
                 @Override
                 public List<AbstractPreferenceController> getPreferenceControllers(Context context) {
                     return buildPreferenceControllers(context, null /* fragment */,
-                            null /* callback */, null /* lifecycle */);
+                            null /* callback */, null /* incCallback */, null /* lifecycle */);
                 }
 
                 @Override